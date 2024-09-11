7e61e783f1af330f8bddcca86c149292e1fb91d5
MarkvdKort <markvanderkort@gmail.com>
2024-09-11 09:23:50 +0200

    feat: setup file structure and flavors

---
+++ android/app/src/main/kotlin/com/example/wilde_buren/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.wilde_buren
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:wilde_buren/app.dart';
+
+void main() {
+  runApp(const App());
+}
